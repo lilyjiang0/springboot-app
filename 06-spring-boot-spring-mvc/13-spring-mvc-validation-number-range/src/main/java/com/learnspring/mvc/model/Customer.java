e4c305f31e1924799911c81da26068d670804401
Yatong Jiang <yatong.jiang@outlook.com>
2025-10-09 21:30:51 +1100

    number range and regular expression validation

---
+++ 06-spring-boot-spring-mvc/13-spring-mvc-validation-number-range/src/main/java/com/learnspring/mvc/model/Customer.java
@@ -0,0 +1,40 @@
+package com.learnspring.mvc.model;
+
+import jakarta.validation.constraints.*;
+
+public class Customer {
+    private String firstName;
+
+    @NotNull(message = "is required")
+    @Size(min=1, message = "is required")
+    private String lastName;
+
+    @Min(value = 0, message = "must be >= 0")
+    @Max(value = 10, message = "must be <= 10")
+    private int freePasses;
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public int getFreePasses() {
+        return freePasses;
+    }
+
+    public void setFreePasses(int freePasses) {
+        this.freePasses = freePasses;
+    }
+
+}
---
+++ 06-spring-boot-spring-mvc/14-spring-mvc-validation-regular-expression/src/main/java/com/learnspring/mvc/model/Customer.java
@@ -0,0 +1,50 @@
+package com.learnspring.mvc.model;
+
+import jakarta.validation.constraints.*;
+
+public class Customer {
+    private String firstName;
+
+    @NotNull(message = "is required")
+    @Size(min=1, message = "is required")
+    private String lastName;
+
+    @Min(value = 0, message = "must be >= 0")
+    @Max(value = 10, message = "must be <= 10")
+    private int freePasses;
+
+    @Pattern(regexp = "^[a-zA-Z0-9]{4}", message = "only 4 characters/ digits")
+    private String postcode;
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public int getFreePasses() {
+        return freePasses;
+    }
+
+    public void setFreePasses(int freePasses) {
+        this.freePasses = freePasses;
+    }
+
+    public String getPostcode() {
+        return postcode;
+    }
+
+    public void setPostcode(String postcode) {
+        this.postcode = postcode;
+    }
+}

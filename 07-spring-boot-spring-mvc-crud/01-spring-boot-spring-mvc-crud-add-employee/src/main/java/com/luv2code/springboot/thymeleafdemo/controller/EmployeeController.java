c76ac5eefabccec97961ceb722e0629dff869687
Yatong Jiang <yatong.jiang@outlook.com>
2025-10-14 23:46:56 +1100

    crud thymeleaf - add employee btn

---
+++ 07-spring-boot-spring-mvc-crud/01-spring-boot-spring-mvc-crud-add-employee/src/main/java/com/luv2code/springboot/thymeleafdemo/controller/EmployeeController.java
@@ -0,0 +1,32 @@
+package com.luv2code.springboot.thymeleafdemo.controller;
+
+import com.luv2code.springboot.thymeleafdemo.entity.Employee;
+import com.luv2code.springboot.thymeleafdemo.service.EmployeeService;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
+
+@Controller
+@RequestMapping("/employees")
+public class EmployeeController {
+    private EmployeeService employeeService;
+
+    public EmployeeController(EmployeeService employeeService) {
+        this.employeeService = employeeService;
+    }
+
+    // Add mapping for listing employees.
+    @GetMapping("/list")
+    public String listEmployees(Model model) {
+        // Get employees from db.
+        List<Employee> employees = employeeService.findAll();
+        // Add to the spring model.
+        model.addAttribute("employees", employees);
+
+        return "employees/list-employees";
+
+    }
+}

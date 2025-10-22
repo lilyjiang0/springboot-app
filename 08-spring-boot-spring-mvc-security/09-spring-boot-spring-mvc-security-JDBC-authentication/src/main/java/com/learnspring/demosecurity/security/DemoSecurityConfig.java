dd20f7a364ab8dd62f314210aa955070064e5407
Yatong Jiang <yatong.jiang@outlook.com>
2025-10-22 22:29:36 +1100

    spring security jdbc authentication, bcrupt and custom tables

---
+++ 08-spring-boot-spring-mvc-security/09-spring-boot-spring-mvc-security-JDBC-authentication/src/main/java/com/learnspring/demosecurity/security/DemoSecurityConfig.java
@@ -0,0 +1,42 @@
+package com.learnspring.demosecurity.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.provisioning.JdbcUserDetailsManager;
+import org.springframework.security.provisioning.UserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+import javax.sql.DataSource;
+
+@Configuration
+public class DemoSecurityConfig {
+    // Add support for JDBC ... no more hardcoded users.
+    @Bean
+    public UserDetailsManager userDetailsManager(DataSource dataSource) {
+        return new JdbcUserDetailsManager(dataSource);
+    }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http.authorizeHttpRequests(configurer ->
+                configurer
+                        .requestMatchers("/").hasRole("EMPLOYEE")
+                        .requestMatchers("/leaders/**").hasRole("MANAGER")
+                        .requestMatchers("/systems/**").hasRole("ADMIN")
+                        .anyRequest().authenticated()
+                )
+                .formLogin(form ->
+                        form
+                                .loginPage("/my-login")
+                                .loginProcessingUrl("/authenticateTheUser")
+                                .permitAll()
+                )
+                .logout(logout -> logout.permitAll()
+                )
+                .exceptionHandling(configurer
+                        -> configurer.accessDeniedPage("/access-denied"))
+        ;
+        return http.build();
+    }
+}
---
+++ 08-spring-boot-spring-mvc-security/10-spring-boot-spring-mvc-security-JDBC-authentication-custom-table/src/main/java/com/learnspring/demosecurity/controller/LoginController.java
@@ -0,0 +1,17 @@
+package com.learnspring.demosecurity.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class LoginController {
+    @GetMapping("/my-login")
+    public String showMyLoginPage() {
+        return "fancy-login";
+    }
+
+    @GetMapping("/access-denied")
+    public String accessDenied() {
+        return "access-denied";
+    }
+}

8b97c993c5712c4c9ea16c681ca39ce754051b65
Yatong Jiang <yatong.jiang@outlook.com>
2025-11-04 14:48:42 +1100

    change one to many fetch type from lazy to eager

---
+++ 09-spring-boot-jpa-advanced-mappings/07-jpa-one-to-many-fetch-type/src/main/java/com/learnspring/cruddemo/entity/InstructorDetail.java
@@ -0,0 +1,82 @@
+package com.learnspring.cruddemo.entity;
+
+import jakarta.persistence.*;
+
+// Annotate the class as an entity and map to db table.
+@Entity
+@Table(name="instructor_detail")
+public class InstructorDetail {
+    // Define the fields.
+    // Annotate the fields with db column names.
+    @Id
+    @GeneratedValue(strategy= GenerationType.IDENTITY)
+    @Column(name="id")
+    private int id;
+
+    @Column(name="youtube_channel")
+    private String youtubeChannel;
+
+    @Column(name="hobby")
+    private String hobby;
+
+    // Add two-way bidirectional.
+    @OneToOne(
+            mappedBy = "instructorDetail",
+            cascade = {CascadeType.DETACH, CascadeType.MERGE, CascadeType.PERSIST, CascadeType.REFRESH}
+    )
+    private Instructor instructor;
+
+
+    // Create constructors.
+    public InstructorDetail() {
+    }
+
+    public InstructorDetail(String youtubeChannel, String hobby) {
+        this.youtubeChannel = youtubeChannel;
+        this.hobby = hobby;
+    }
+
+    // Generate getter/setter methods.
+    public Instructor getInstructor() {
+        return instructor;
+    }
+
+    public void setInstructor(Instructor instructor) {
+        this.instructor = instructor;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getYoutubeChannel() {
+        return youtubeChannel;
+    }
+
+    public void setYoutubeChannel(String youtubeChannel) {
+        this.youtubeChannel = youtubeChannel;
+    }
+
+    public String getHobby() {
+        return hobby;
+    }
+
+    public void setHobby(String hobby) {
+        this.hobby = hobby;
+    }
+
+    // Generate toString() method.
+
+    @Override
+    public String toString() {
+        return "InstructorDetail{" +
+                "id=" + id +
+                ", youtubeChannel='" + youtubeChannel + '\'' +
+                ", hobby='" + hobby + '\'' +
+                '}';
+    }
+}

8dfb155580c11972d0698bda4160ba7755143fdd
Yatong Jiang <yatong.jiang@outlook.com>
2025-11-09 16:09:47 +1100

    aop @before advice

---
+++ 10-spring-boot-aop/01-spring-boot-aop-demo/src/main/java/com/learnspring/aopdemo/AopdemoApplication.java
@@ -0,0 +1,27 @@
+package com.learnspring.aopdemo;
+
+import com.learnspring.aopdemo.dao.AccountDAO;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+public class AopdemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AopdemoApplication.class, args);
+	}
+
+	@Bean
+	public CommandLineRunner commandLineRunner(AccountDAO accountDAO) {
+		return runner -> {
+			demoTheBeforeAdvice(accountDAO);
+			demoTheBeforeAdvice(accountDAO);
+		};
+	}
+
+	private void demoTheBeforeAdvice(AccountDAO accountDAO) {
+		accountDAO.addAccount();
+	}
+}

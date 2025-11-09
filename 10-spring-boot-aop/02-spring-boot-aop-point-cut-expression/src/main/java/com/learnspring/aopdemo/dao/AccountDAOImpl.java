7eebf2a9fe09cf1c2f5f9150a6255733b4f6791e
Yatong Jiang <yatong.jiang@outlook.com>
2025-11-09 16:38:12 +1100

    point cut expression

---
+++ 10-spring-boot-aop/02-spring-boot-aop-point-cut-expression/src/main/java/com/learnspring/aopdemo/dao/AccountDAOImpl.java
@@ -0,0 +1,11 @@
+package com.learnspring.aopdemo.dao;
+
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class AccountDAOImpl implements AccountDAO {
+    @Override
+    public void addAccount() {
+        System.out.println(getClass() + ": Doing my db work: adding an account");
+    }
+}
---
+++ 10-spring-boot-aop/02-spring-boot-aop-point-cut-expression/src/main/java/com/learnspring/aopdemo/dao/MembershipDAOImpl.java
@@ -0,0 +1,11 @@
+package com.learnspring.aopdemo.dao;
+
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class MembershipDAOImpl implements MembershipDAO {
+    @Override
+    public void addAccount() {
+        System.out.println(getClass() + ": Doing my db work: adding a membership account");
+    }
+}

7334f3297567931a50979ab926ada85f74af596e
Yatong Jiang <yatong.jiang@outlook.com>
2025-11-24 22:19:02 +1100

    slight refactor comment

--- 10-spring-boot-aop/05-spring-boot-aop-point-cut-expression-match-return-type/src/main/java/com/learnspring/aopdemo/aspect/MyDemoLoggingAspect.java
+++ 10-spring-boot-aop/05-spring-boot-aop-point-cut-expression-match-return-type/src/main/java/com/learnspring/aopdemo/aspect/MyDemoLoggingAspect.java
@@ -8,8 +8,10 @@ import org.springframework.stereotype.Component;
 @Component
 public class MyDemoLoggingAspect {
     // Add all related advices for logging.
+
     // Match method starting with "add" and return type is void.
-//    @Before("execution(void add*())")
+    // @Before("execution(void add*())")
+
     // Match method starting with "add" and return type is any.
     @Before("execution(* add*())")
     public void beforeAddAccountAdvice() {

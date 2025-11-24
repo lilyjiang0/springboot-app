9433ba1e4c3ccad80cd51d6e58de19337869dfd1
Yatong Jiang <yatong.jiang@outlook.com>
2025-11-24 21:55:35 +1100

    match return type

---
+++ 10-spring-boot-aop/05-spring-boot-aop-point-cut-expression-match-return-type/src/main/java/com/learnspring/aopdemo/aspect/MyDemoLoggingAspect.java
@@ -0,0 +1,18 @@
+package com.learnspring.aopdemo.aspect;
+
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.springframework.stereotype.Component;
+
+@Aspect
+@Component
+public class MyDemoLoggingAspect {
+    // Add all related advices for logging.
+    // Match method starting with "add" and return type is void.
+//    @Before("execution(void add*())")
+    // Match method starting with "add" and return type is any.
+    @Before("execution(* add*())")
+    public void beforeAddAccountAdvice() {
+        System.out.println("\n ========>>> Executing @Before advice on addAccount()");
+    }
+}

fed32456dab5ae1b2dd82cf193af1f21790d4834
Yatong Jiang <yatong.jiang@outlook.com>
2025-11-27 18:14:59 +1100

    after throwing advice

---
+++ 10-spring-boot-aop/15-spring-boot-aop-after-throwing-advice/src/main/java/com/learnspring/aopdemo/AopdemoApplication.java
@@ -0,0 +1,68 @@
+package com.learnspring.aopdemo;
+
+import com.learnspring.aopdemo.dao.AccountDAO;
+import com.learnspring.aopdemo.dao.MembershipDAO;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import java.util.List;
+
+@SpringBootApplication
+public class AopdemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AopdemoApplication.class, args);
+	}
+
+	@Bean
+	public CommandLineRunner commandLineRunner(AccountDAO accountDAO, MembershipDAO membershipDAO) {
+		return runner -> {
+//			demoTheBeforeAdvice(accountDAO, membershipDAO);
+//			demoAfterReturningAdvice(accountDAO);
+			demoAfterThrowingAdvice(accountDAO);
+		};
+	}
+
+	private void demoAfterThrowingAdvice(AccountDAO accountDAO) {
+		List<Account> accounts = null;
+
+		try {
+			// Add a boolean flag to simulate exceptions.
+			boolean tripWire = true;
+			accounts = accountDAO.findAccounts(tripWire);
+			// Print out accounts.
+			System.out.println("\n Main Program: demo the after throwing advice ");
+			for (Account acc : accounts) {
+				System.out.println(acc);
+			}
+		} catch (Exception e) {
+			System.out.println("\n Main Program: caught exception " + e);
+		}
+
+	}
+
+	private void demoAfterReturningAdvice(AccountDAO accountDAO) {
+		List<Account> accounts = accountDAO.findAccounts();
+		// Print out accounts.
+		System.out.println("\n Main Program: demo the after returning advice ");
+		for (Account acc : accounts) {
+			System.out.println(acc);
+		}
+	}
+
+	private void demoTheBeforeAdvice(AccountDAO accountDAO, MembershipDAO membershipDAO) {
+		Account acc = new Account();
+		acc.setName("Merlin");
+		acc.setLevel("Gold");
+		accountDAO.addAccount(acc, true);
+		accountDAO.doWork();
+		String name = accountDAO.getName();
+		accountDAO.setName("n");
+		String account = accountDAO.getAccount();
+		accountDAO.setAccount("a");
+		membershipDAO.addMemberAccount();
+		membershipDAO.getMembershipPoint();
+	}
+}
